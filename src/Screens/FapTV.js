52f8c459495868f29eb447d5cf2d8faa9c16fb37
thanhnam218 <nthanhnam0821@gmail.com>
2023-11-11 16:39:54 +0700

    xongUIchannelsub

---
+++ src/Screens/FapTV.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {AntDesign, EvilIcons, FontAwesome5} from '@expo/vector-icons'
+import Header from '../Component/Header';
+
+export default function FapTV() {
+  return (
+    <View style={styles.container}>
+        <View>
+          <Header/>
+          <View>
+          <Image source={require('../image/FapTVLogo.png')} style={{}}></Image>
+          <Text style={{fontSize:30, }}><b>FAPTV</b></Text>
+          </View>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'white'
+    },
+  });
+  
